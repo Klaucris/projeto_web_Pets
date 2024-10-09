1ee61db35fce57d8bb8b218f4db90c909633844a
Klaucris <159507043+Klaucris@users.noreply.github.com>
2024-10-09 07:03:45 -0300

    atualizacao2

--- Program.cs
+++ Program.cs
@@ -7,14 +7,22 @@ public class Program
         var builder = WebApplication.CreateBuilder(args);
         var app = builder.Build();
 
-        app.MapGet("/", () => "Projeto Web - LH Pets versão 1!");
+        app.MapGet("/", () => "Projeto Web - LH Pets versão 1");
+
         app.UseStaticFiles();
-        app.MapGet("/index",(HttpContext contexto)=>{
+
+        app.MapGet("/index",(HttpContext contexto)=> {
+
             contexto.Response.Redirect("index.html", false);
+
         });
+
         Banco dba=new Banco();
+
         app.MapGet("/listaClientes", (HttpContext contexto) =>{
-            contexto.Response.WriteAsync( dba.GetListastring();)
+        
+            contexto.Response.WriteAsync( dba.GetListaString());
+            
         });
 
         app.Run();
